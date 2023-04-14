1296d61a33bce481d38a08add3ca53cab824b61d
zhoupeng <1875112921@qq.com>
2023-04-14 15:21:07 +0800

    优化

--- build/build-prod.js
+++ build/build-prod.js
@@ -8,7 +8,7 @@ const rm = require('rimraf')
 const path = require('path')
 const chalk = require('chalk')
 const webpack = require('webpack')
-const config = require('../config/index')
+const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
 
 const spinner = ora('building for production...')
@@ -22,7 +22,7 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     process.stdout.write(stats.toString({
       colors: true,
       modules: false,
-      children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
+      children: false, // If you are using ts-loader, setting this to true will make TypeScript errors showAppDetail up during build.
       chunks: false,
       chunkModules: false
     }) + '\n\n')
